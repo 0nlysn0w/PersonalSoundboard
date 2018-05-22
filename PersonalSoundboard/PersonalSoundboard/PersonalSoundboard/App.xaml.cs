835fc70f61a117173fb81e6eb815f6fad2f718b8
Alexander van Woensel <alexander_hockey@msn.com>
2018-05-22 14:30:18 +0200

    Add layout from personal project

--- PersonalSoundboard/PersonalSoundboard/PersonalSoundboard/App.xaml.cs
+++ PersonalSoundboard/PersonalSoundboard/PersonalSoundboard/App.xaml.cs
@@ -1,22 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
+using System;
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
+[assembly: XamlCompilation (XamlCompilationOptions.Compile)]
 namespace PersonalSoundboard
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App ()
+        {
+            InitializeComponent();
 
-			MainPage = new GridPage();
-		}
+            MainPage = new NavigationPage(new MainPage());
+        }
 
-		protected override void OnStart ()
+        protected override void OnStart ()
 		{
 			// Handle when your app starts
 		}
--- PersonalSoundboard/PersonalSoundboard/PersonalSoundboard/RecordSound.xaml.cs
+++ PersonalSoundboard/PersonalSoundboard/PersonalSoundboard/RecordSound.xaml.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
 namespace PersonalSoundboard
 {
-	public partial class MainPage : ContentPage
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class RecordSound : ContentPage
 	{
-		public MainPage()
+		public RecordSound ()
 		{
-			InitializeComponent();
+			InitializeComponent ();
 		}
         private async void NavigateButton_OnReleased(object sender, EventArgs e)
         {
-            await Navigation.PushAsync(new RecordSound());
+            await Navigation.PushAsync(new AddPicAndTitle());
         }
-	}
+    }
 }
